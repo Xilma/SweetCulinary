ab8f78b2472d41116336f0b03eb10c85d254398e
Xilma <atiruo@gmail.com>
2018-11-19 22:22:38 +0300

    Initial commit

---
+++ app/src/main/java/android/example/com/sweetculinary/views/MainActivity.java
@@ -0,0 +1,14 @@
+package android.example.com.sweetculinary.views;
+
+import android.example.com.sweetculinary.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
